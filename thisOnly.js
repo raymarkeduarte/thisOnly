b4b366b07780bbab6c6fd40ca08f0dec3f9a11c3
Raymark E. Dela Cruz <raymarkedelacruz@gmail.com>
2023-02-13 23:31:21 +0800

    initial commit

---
+++ thisOnly.js
@@ -0,0 +1,67 @@
+/**
+ * @description limit the characters the user can input
+ * @author RED
+ */
+$(document).ready(function(){
+    $('[numOnly]').each(function(){
+        $(this).on('keypress', function(e){
+            // get value
+            let val = $(this).attr('numOnly');
+            // check if case sensitive
+            const isSensitive = (val.at(0) === "(" && val.at(-1) === ")");
+            val = (isSensitive) ? val : val.toLowerCase();
+            // convert value of letOnly attribute to an array
+            const esc = val.split("");
+            // pressed key
+            const key = e.which || e.keyCode;
+            let code = String.fromCharCode(key);
+            code = (isSensitive) ? code : code.toLowerCase();
+            const notNum = (key > 31 && (key < 48 || key > 57));
+            const escHasNum = (esc.includes(code) && !notNum)
+            // allow non-numeric and custom keys defined
+            if( notNum && esc.includes(code) ) return;
+
+            // prevent keypress events of non-numeric or numerical numbers that are defined in the attribute
+            if( notNum || escHasNum ) e.preventDefault();
+        });
+    });
+    $('[letOnly]').each(function(){
+        $(this).on('keypress', function(e){
+            // get value
+            let val = $(this).attr('letOnly');
+            // check if case sensitive
+            const isSensitive = (val.at(0) === "(" && val.at(-1) === ")");
+            val = (isSensitive) ? val : val.toLowerCase();
+            // convert value of letOnly attribute to an array
+            const esc = val.split("");
+            // pressed key
+            const key = e.which || e.keyCode;
+            let code = String.fromCharCode(key);
+            code = (isSensitive) ? code : code.toLowerCase();
+            const notLet = (code > 31 && ( (key < 65 || key > 90) && (key < 97 || key > 122) ) );
+            const escHasLet = (esc.includes(code) && !notLet)
+            // allow non-latin alphabet and custom keys defined
+            if( notLet && esc.includes(code) ) return;
+
+            // prevent keypress events of non-latin or latin alphabets that are defined in the attribute
+            if( notLet || escHasLet ) e.preventDefault();
+        });
+    });
+    $('[thisOnly]').each(function(){
+        $(this).on('keypress', function(e){
+            // get value
+            let val = $(this).attr('thisOnly');
+            // check if case sensitive
+            const isSensitive = (val.at(0) === "(" && val.at(-1) === ")");
+            val = (isSensitive) ? val : val.toLowerCase();
+            // convert value of letOnly attribute to an array
+            const esc = val.split("");
+            // pressed key
+            const key = e.which || e.keyCode;
+            let code = String.fromCharCode(key);
+            code = (isSensitive) ? code : code.toLowerCase();
+            // only allow characters defined
+            if( !esc.includes(code) ) e.preventDefault();
+        });
+    });
+});
